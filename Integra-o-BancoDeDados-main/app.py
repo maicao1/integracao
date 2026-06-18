bfc3b56b0f68e7f757f70502510037c1d20dd7f8
maicao1 <maicon.carolino.campos@escola.pr.gov.br>
2026-06-18 09:52:26 -0300

    Add files via upload

---
+++ Integra-o-BancoDeDados-main/app.py
@@ -0,0 +1,46 @@
+# Import do framework flask
+# Import do render_template para ler o HTML e busca ou o endereço do arquivo ou a URL
+# request para capturar os dados
+from flask import Flask, render_template, request
+
+import mysql.connector 
+
+# Para vincular as páginas e saberem ond estão:
+
+app = Flask(__name__)
+
+# Cria conexão com o mySQL
+bd_config = {
+    'host': '127.0.0.1',
+    'user': 'root',
+    'password': 'escola',
+    'database': 'cadastro1'
+}
+
+# Criação de rota para o arquivo HTML principal
+
+@app.route('/cadastrar', methods=['POST'])
+def criar_cadastro():
+
+    cpf = request.form['cpf']
+    primeiro_nome = request.form['primeiro_nome']
+    sobrenome = request.form['sobrenome']
+    idade = request.form['idade']
+
+
+  conexao = mysql.connector.connect(**bd_config)
+
+  curso = conexao.cursor()
+
+  query = "INSERT INTO cliente1(CPF,PRIMEIRO_NOME,SOBRENOME,IDADE)VALUES(%S,%S,%S)"
+
+
+
+
+
+def indexRota():
+    return render_template('index.html')
+
+@app.route('/cadastrar', methods=['POST'])
+# Biblioteca mysql.connector conecta o Python com o MySQL
+# decorador tem @
